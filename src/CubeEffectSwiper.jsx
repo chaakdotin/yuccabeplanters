4160c096220c0d8966996692e31fc8e167d097fe
Paru29 <59466619+Paru29@users.noreply.github.com>
2025-03-13 05:08:46 +0530

    sdfsdfdsf

--- src/CubeEffectSwiper.jsx
+++ src/CubeEffectSwiper.jsx
@@ -1,9 +1,10 @@
 import React, { useRef, useState, useEffect } from "react";
+import { useGSAP } from '@gsap/react';
 import { gsap, ScrollTrigger, ScrollToPlugin } from "gsap/all";
 import ScrollSections from "./ScrollSections";
 import './new.css';
 import Carousel from './Carousel';
-gsap.registerPlugin( ScrollTrigger, ScrollToPlugin);
+gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollToPlugin);
 const CubeEffectSwiper = ({scrollToSection}) => {
     const { refs, setCurrentPage } = scrollToSection;
     const swiperRef = useRef(null);
@@ -11,7 +12,22 @@ const CubeEffectSwiper = ({scrollToSection}) => {
     var i = 0;
     
     useEffect(() => {
-
+        const tl1 = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".stagger", // Element that triggers the animation
+                start: "top top",     // When the top of `.start-class` hits the center of the viewport/ Element that defines the end point
+                end: "bottom bottom",    // When the bottom of `.end-class` hits the center of the viewport
+                scrub: true,
+                markers: true
+            }
+        });
+        tl1.to(".stagger1 h1", {
+            opacity: 1,
+            transform: "none",
+            transitionDuration : '.8s',
+            stagger: 0.05,
+            duration: 1
+        });
         const lt = gsap.timeline({
             scrollTrigger: {
                 trigger: ".YUCCABE", // Element that triggers the animation
@@ -86,10 +102,12 @@ const CubeEffectSwiper = ({scrollToSection}) => {
     return (
         <div className="scroll-container" style={{ width: "100vw", overflow: "hidden" }} >
             <Carousel data={{refs, setCurrentPage}} />
-            <div style={{ zIndex: 1 }} className="h-100vh bg-color position-relative h-100vh"  ref={refs.about}>
+            <div style={{ zIndex: 1 }} className="h-100vh bg-color position-relative h-100vh stagger"  ref={refs.about}>
                 <div className='d-flex flex-column gap-5 align-items-center justify-content-center h-100 border-reduis'>
-                    <div>
-                        <h1 className='text-white secrion-3-font poppins-medium'>co-create your green spaces with us</h1>
+                    <div className="stagger1">
+                        <h1 className='text-white secrion-3-font poppins-medium'>co-create </h1>
+                        <h1 className='text-white secrion-3-font poppins-medium'>your green</h1>
+                        <h1 className='text-white secrion-3-font poppins-medium'>spaces with us</h1>
                     </div>
                 </div>
             </div>
