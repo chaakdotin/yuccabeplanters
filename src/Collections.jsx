407da486c3bacd55e3ff850aad544c881bae0dfa
Praveen Prasad <praveenpatkuwal29@gmail.com>
2025-05-11 11:24:15 +0530

    dfgdfg

--- src/Collections.jsx
+++ src/Collections.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useRef,useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import lenis  from './LenisWrapper'
 import ScrollTrigger from "gsap/ScrollTrigger";
